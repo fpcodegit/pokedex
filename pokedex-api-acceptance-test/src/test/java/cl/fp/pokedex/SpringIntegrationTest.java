6780218f9730ca152dc639e1bfdaed7318bbb1a7
fp <******@gmail.com>
2021-07-06 19:07:06 -0400

    Add wiremock and stubs responses

--- pokedex-api-acceptance-test/src/test/java/cl/fp/pokedex/SpringIntegrationTest.java
+++ pokedex-api-acceptance-test/src/test/java/cl/fp/pokedex/SpringIntegrationTest.java
@@ -2,8 +2,10 @@ package cl.fp.pokedex;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = PokedexApplication.class)
+@AutoConfigureWireMock
 public class SpringIntegrationTest {
 }
